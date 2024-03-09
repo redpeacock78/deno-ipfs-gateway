83ed61bd9d4c8e56b10d979b07b216003ec49180
redpeacock78 <scarletpeacock78@gmail.com>
2024-03-09 20:27:27 +0900

    fix: Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ALLOW_ORIGINS="${ALLOW_ORIGINS:?ALLOW_ORIGINS is required}"
-[ -z "${ALLOW_ORIGINS}" ] && echo "ALLOW_ORIGINS is required" && exit 1
+[[ -z "${ALLOW_ORIGINS}" ]] && echo "ALLOW_ORIGINS is required" && exit 1
 
 echo "Starting IPFS Gateway..."
 
 CONTAINER_NAME="$(curl -s --unix-socket /run/docker.sock "http://docker/containers/${HOSTNAME}/json" | jq -r .Name)"
 DOCKER_SCALE_NUM="$(echo "${CONTAINER_NAME}" | awk -F"_" '{print $NF}')"
-[ "${DOCKER_SCALE_NUM}" = "${CONTAINER_NAME}" ] && DOCKER_SCALE_NUM="$(echo "${CONTAINER_NAME}" | awk -F"-" '{print $NF}')"
-[ "${DOCKER_SCALE_NUM}" = "${CONTAINER_NAME}" ] && DOCKER_SCALE_NUM="1"
+[[ "${DOCKER_SCALE_NUM}" == "${CONTAINER_NAME}" || ! "${DOCKER_SCALE_NUM}" =~ ^[1-9]+$ ]] &&
+  DOCKER_SCALE_NUM="$(echo "${CONTAINER_NAME}" | awk -F"-" '{print $NF}')"
+[[ "${DOCKER_SCALE_NUM}" == "${CONTAINER_NAME}" || ! "${DOCKER_SCALE_NUM}" =~ ^[1-9]+$ ]] &&
+  DOCKER_SCALE_NUM="1"
 IPFS_DIR="/ipfs/${DOCKER_SCALE_NUM}"
 
 mkdir -p "${IPFS_DIR}"
